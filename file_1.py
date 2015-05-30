567a770ec56513875e3ce1525dea056ad66ef0ee
William Sankey <WSankey@gmail.com>
2015-05-29 21:34:03 -0400

    Updated analysis file.

--- file_1.py
+++ file_1.py
@@ -1,18 +1,36 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("test.csv", nrows=1000000)
-print df.head(10)
+df = pd.read_csv("test.csv")
+df = df[['npi', 'total_drug_cost', 'nppes_provider_last_org_name', 'nppes_provider_city', 'nppes_provider_state', 'generic_name']]
+
 def opiods(series):
     if series["generic_name"]=="FENTANYL":
+        return 1
+    elif series["generic_name"]=="METHADONE HCL":
+        return 1
+    elif series["generic_name"]=="MORPHINE SULFATE":
+        return 1
+    elif series["generic_name"]=="OXYMORPHONE HCL":
         return 1
     else:
         return 0
 
 df['opioid_var'] = df.apply(opiods, axis=1)
-newdf = df[df.opioid_var==1]
+df = df.loc[(df.opioid_var==True)]
 
+df['total_drug_cost'] = df['total_drug_cost'].fillna(0)
+df['total_drug_cost'] = df['total_drug_cost'].str.strip("$")
+df['total_drug_cost'] = df['total_drug_cost'].str.replace(',','')
+df['total_drug_cost'] = df['total_drug_cost'].astype(float)
+
+df['tot'] = df['total_drug_cost'].groupby(df['npi']).transform('sum')
+df = df.drop_duplicates(cols='npi')
+#df['total_drug_cost'] = df['total_drug_cost'].astype(int)
+a =  df.sort('tot', ascending=False, axis=0)[:100]
+print a
+#print newdf.head(10)
 #print DataFrame.dtypes
 #grouped = file_a.groupby('npi')['bene_count'].agg(lambda col: col.idxmax())[:100]
-print newdf.head(10)
+#print newdf.head(100)
 #print grouped.head()
