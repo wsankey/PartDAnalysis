3a15ed0d3b9ef5b63529bf2d20714163d585426d
William Sankey <WSankey@gmail.com>
2015-05-30 22:50:25 -0400

    Updated file.

--- file_1.py
+++ file_1.py
@@ -28,7 +28,8 @@ df['tot'] = df['total_drug_cost'].groupby(df['npi']).transform('sum')
 df = df.drop_duplicates(cols='npi')
 #df['total_drug_cost'] = df['total_drug_cost'].astype(int)
 a =  df.sort('tot', ascending=False, axis=0)[:100]
-print a
+a = a[['npi', 'tot', 'nppes_provider_last_org_name', 'nppes_provider_first_name', 'nppes_provider_city', 'nppes_provider_state']]
+a.to_csv('out.csv')
 #print newdf.head(10)
 #print DataFrame.dtypes
 #grouped = file_a.groupby('npi')['bene_count'].agg(lambda col: col.idxmax())[:100]
